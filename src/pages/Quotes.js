ccb99332370b415318ccf78a66c9fc989c07ac06
Erin Wise <erinwise88@gmail.com>
2023-11-01 21:38:04 -0400

    styling for quotes

--- src/pages/Quotes.js
+++ src/pages/Quotes.js
@@ -2,26 +2,36 @@ import React, {useEffect, useState} from "react"
 
 function Quotes() {
 
-const [quote, setQuote] = useState("Hello")
-const [currentIndex, setCurrentIndex] = useState(0)
+const [quote, setQuote] = useState("")
+const [author, setAuthor] = useState("")
+
 
 // useEffect(() => {
-//     fetch("https://type.fit/api/quotes")
+//     fetch("https://api.quotable.io/random")
 //     .then(res => res.json())
-//     .then(data => setQuote(data))
-// })
+//     .then(quotes => setQuote(quotes.content))
+// }, [])
 
+//https://api.quotable.io/random
 function handleClick() {
-
+    fetch("https://api.quotable.io/random")
+    .then(res => res.json())
+    .then(quotes => {
+        setQuote(quotes.content)
+        setAuthor(quotes.author)})
 }
 
 return (
-    <div>
+    <div style={{ textAlign: 'center' }}>
+        <div className="quote-heading">
+            <h1>Start Thinking Like a College Kid!</h1>
+        </div>
         <div className="button-container">
             <button className="button-53" role="button" onClick={handleClick}>Click for Quote</button>
         </div>
-        <div className="quote-container">
+        <div className="quote-container" >
                 <h3>{quote}</h3>
+                <small>{author}</small>
         </div>
     </div>
 )
