cb081d2eb2ba79486655cb15118715ffd1bfb7b6
Erin Wise <erinwise88@gmail.com>
2023-11-02 21:27:47 -0400

    used ui semantics for task form

--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react"
 import Task from "./Task"
+import { Form, Input } from "semantic-ui-react";
 
 function TaskForm() {
 
@@ -38,7 +39,43 @@ function handleDeleteTask(deletedTask) {
 
  return (
     <div className="TaskList">
-        <div className="TaskForm">
+        <h3>Add a New Task</h3>
+        <Form onSubmit={handleSubmit}>
+            <Form.Field inline >
+                {/* <label>New Task</label> */}
+               <Form.Input label="New Task" 
+                placeholder="Enter Task" 
+                value={newTaskField}
+                onChange={(e) => setNewTaskField(e.target.value)}/>
+                
+                {/* <Input placeholder="Task" 
+                    name="task" 
+                    value={newTaskField} 
+                    onChange={(e) => setNewTaskField(e.target.value)}/> */}
+            </Form.Field>
+            <Form.Button>Submit</Form.Button>
+
+
+        </Form>
+        
+    <br/>
+    <div>
+        <h3>My Tasks</h3><br/>
+    </div>
+    <div className="Items">
+        {tasks.map((item) => (
+            <Task key={item.id} item={item} onDeleteTask={handleDeleteTask}/>
+        ))}
+    </div>
+    </div>
+
+ )
+}
+
+export default TaskForm
+
+//old form
+{/* <div className="TaskForm">
             <h3 >Add New Task</h3>
             <form onSubmit={handleSubmit}>
                 <input 
@@ -52,18 +89,4 @@ function handleDeleteTask(deletedTask) {
                     value="Submit">
                 </input>
             </form>
-        </div>
-    <div>
-        <h3>My Tasks</h3>
-    </div>
-    <div className="Items">
-        {tasks.map((item) => (
-            <Task key={item.id} item={item} onDeleteTask={handleDeleteTask}/>
-        ))}
-    </div>
-    </div>
-
- )
-}
-
-export default TaskForm
+        </div> */}
